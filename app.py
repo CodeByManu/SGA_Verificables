e7b66dc57a218faf4ec2b120a6d9bcf19da8e176
Manuel Tagle <mjtagle2@miuandes.cl>
2025-04-11 21:12:02 -0400

    feat: course list view & navbar implemented

--- app.py
+++ app.py
@@ -10,8 +10,37 @@ with app.app_context():
     db.create_all()
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html', active_page='home')
+
+@app.route('/courses', methods=['GET'])
+def get_courses():
+    courses = Course.query.all()
+    return render_template('courses.html', courses=courses, active_page='courses')
+
+@app.route('/students', methods=['GET'])
+def get_students():
+    students = Student.query.all()
+    return render_template('students.html', students=students, active_page='students')
+
+@app.route('/teachers', methods=['GET'])
+def get_teachers():
+    teachers = Teacher.query.all()
+    return render_template('teachers.html', teachers=teachers, active_page='teachers')
+
+@app.route('/courses/new', methods=['GET','POST'])
+def post_course():
+    if request.method == 'POST':
+        code = request.form.get('code')
+        name = request.form.get('name')
+        description = request.form.get('description')
+        new_course = Course(code=code, name=name, description=description)
+        db.session.add(new_course)
+        db.session.commit()
+        flash('Course added successfully!')
+        return redirect(url_for('get_courses'))
+    return render_template('new_course.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
+    DEBUG = True
+    TEMPLATES_AUTO_RELOAD = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'miy_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://sga_user:sga_user@localhost/sga_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
